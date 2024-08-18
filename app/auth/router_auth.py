3bf22730cf96c0e24c54bf01a25c9f464e9c5c54
VoblaSuperFish <kuznec112233@mail.ru>
2024-08-18 22:06:34 +0500

    Confirm routers authorize

--- app/auth/router_auth.py
+++ app/auth/router_auth.py
@@ -7,7 +7,11 @@ from .utils import data_validate, creating_dict_for_profile
 
 
 
-router_auth = Blueprint('router_auth', __name__, static_folder='static', template_folder='templates')
+router_auth = Blueprint('router_auth', 
+    __name__, 
+    static_folder='static', 
+    template_folder='templates'
+    )
 
 
 def create_user_session(*, user):
@@ -17,7 +21,7 @@ def create_user_session(*, user):
     userLogin = UserLogin().create(user)
     rm = True if request.form.get('remainme') else False
     login_user(userLogin, remember=rm)
-    return redirect(url_for('.profile'))    
+    return 200
 
 
 def function_by_login():
@@ -27,7 +31,8 @@ def function_by_login():
     if user_from_orm:
         password_valid = orm.validate_password_user(user_from_orm['psw'], request.form['psw'])
         if password_valid:
-            create_user_session(user=user_from_orm)
+            result = create_user_session(user=user_from_orm)
+            return result
         else:
             flash('Неверная пара email/пароль', category='error')
     else:
@@ -44,7 +49,8 @@ def function_by_register():
             orm.register_user(request.form['username'], request.form['email'], request.form['psw'])
             #Получение пользователя из базы по его email и создание 
             user_from_orm = orm.get_user_by_email(request.form['email'])
-            create_user_session(user=user_from_orm)
+            result = create_user_session(user=user_from_orm)
+            return result
         else:
             flash('Пользователь с таким email уже есть.', category='error')
     except Exception as error:
@@ -54,22 +60,36 @@ def function_by_register():
 
 @router_auth.route('/login', methods=['POST', 'GET'])
 def login():
+    """
+    Обработчик запроса на получение страницы аутентификации и обработчик
+    данных с формы, которую заполняет пользователь (mail, psw), при корректных данных,
+    выдает сессию и возвращает redirect на профиль пользователя.
+    """
     #Если пользователь уже авторизован
     if current_user.is_authenticated:
         return redirect(url_for('.profile'))
     if request.method == 'POST':
-        function_by_login()
+        result = function_by_login()
+        if result == 200:
+            return redirect(url_for('.profile'))
     return render_template('auth/login.html', title='Вход')
 
 
 
 @router_auth.route('/register', methods=['POST', 'GET'])
 def register():
+    """
+    Обработчик запроса на получение страницы регистрации и обработка данных с формы, 
+    полученных со страницы (username, mail, psw1, psw2), при валидных данных выдает
+    сессию пользователю и перекидывает его на профиль.
+    """
     if current_user.is_authenticated:
         return redirect(url_for('.profile'))
     if request.method == 'POST':
         if data_validate(request.form) == 200:
-            function_by_register()
+            result = function_by_register()
+            if result == 200:
+                return redirect(url_for('.profile'))
         elif data_validate(request.form) == 400:
             flash("Неккоректные email или username", category='error')
         else:
@@ -79,6 +99,11 @@ def register():
 @login_required
 @router_auth.route('/logout', methods=['GET'])
 def logout():
+    """
+    Обработчик запроса на выход из аккаунта, доступен тем пользователям, 
+    у которых есть сессия. 
+    Забирает сессию у пользователя и возвращает redirect на url функции аутентификации.
+    """
     logout_user()
     flash("Вы вышли из аккаунта", category='success')
     return redirect(url_for('.login'))
@@ -86,7 +111,11 @@ def logout():
 
 @login_required
 @router_auth.route('/profile', methods=['GET'])
-def profile(): 
+def profile():
+    """
+    Получение своего профиля, доступен только пользователям с сессией.
+    Формирует данные о пользователе и возвращает их в html странице.
+    """
     orm = OrmRequest()
     user_email = current_user.get_email()
     user_info = orm.get_user_by_email(user_email)
