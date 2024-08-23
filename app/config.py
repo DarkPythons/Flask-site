722f02af3029cd93c22c44560846ec45fa9464bc
DarkPythons <kuznec112233@mail.ru>
2024-08-24 00:21:09 +0500

    Add post handler in converter

--- app/config.py
+++ app/config.py
@@ -1,11 +1,11 @@
-import os
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict  #type: ignore
 from dotenv import load_dotenv, find_dotenv
 from pydantic import Field
 
 load_dotenv(find_dotenv())
 
 class BaseConfigClassInFile(BaseSettings):
+    """Родительский класс, где указываем настройки, как и откуда будем брать данные из окружения"""
     model_config = SettingsConfigDict(
         env_file='.env',
         env_file_encoding='utf-8',
@@ -14,6 +14,8 @@ class BaseConfigClassInFile(BaseSettings):
     )
 
 class BaseSettingsDataBase(BaseConfigClassInFile):
+    """Указывем базовые настройки для подключения к базе данных (Postgresql)"""
+
     #Данные в default представлены в виде примера
     DB_USER:str = Field(min_length=1, max_length=30, default="postgres")
     DB_PASS:str = Field(min_length=5, max_length=100)
@@ -22,11 +24,24 @@ class BaseSettingsDataBase(BaseConfigClassInFile):
     DB_NAME:str = Field(min_length=1, max_length=100, default='praktika_flask')
 
     def get_db_url(self):
+        """Метод возвращает url для подключения к базе данных, по настройкам из окружения,
+        данные из которого берутся выше"""
         return f"postgresql+psycopg2://\
 {self.DB_USER}:{self.DB_PASS}@\
 {self.DB_HOST}:{self.DB_PORT}/\
 {self.DB_NAME}"
 
 class BaseSettingsApp(BaseConfigClassInFile):
+    """Класс для взятия данных из окружения, для настройки приложения в целом"""
     PROJECT_ON_DEBUG:bool = Field(default=True)
     SECRET_KEY:str = Field(min_length=5, max_length=50)
+
+headers = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36"
+    }
+
+class SettingByAPICurrency(BaseConfigClassInFile):
+    """Класс для взятия данных из окружения, для настройки API валюты"""
+    KEY_CURRENCY_API:str = Field(min_length=3)
+    HEADERS_BY_REQ:dict = Field(default=headers)
--- app/converter/router_converter.py
+++ app/converter/router_converter.py
@@ -1,4 +1,8 @@
 from flask import Blueprint, request, render_template, flash
+from config import SettingByAPICurrency
+import requests
+
+setting_currency = SettingByAPICurrency()
 
 converter_router = Blueprint('converter_router', __name__,
     static_folder='static', 
@@ -7,5 +11,31 @@ converter_router = Blueprint('converter_router', __name__,
 @converter_router.route('/', methods=['GET', 'POST'])
 def converter():
     if request.method == 'POST':
-        pass
+        api_key = setting_currency.KEY_CURRENCY_API
+        from_valute = request.form['from_c']
+        to_valut = request.form['to_c']
+        amount = request.form['amount']
+        url = f'https://api.getgeoapi.com/v2/currency/convert?api_key={api_key}&from={from_valute}&to={to_valut}&amount={amount}&format=json'
+        response = requests.get(url, headers=setting_currency.HEADERS_BY_REQ)
+        response_json = response.json()
+        # Код введенной валюты от которой должна быть конвертация
+        from_c_code = response_json['base_currency_code']
+        # Имя введеной валюты
+        from_c_name = response_json['base_currency_name']
+        # Введенное количество валюты
+        c_amount = response_json['amount']
+        c_amount = round(float(c_amount), 2)
+        # Последнее обновление данных
+        update_date = response_json['updated_date']
+
+        # Имя валюты, в которую нужно было конвертировать
+        to_c_name = response_json['rates'][to_valut]['currency_name']
+        # Стоимость одной единицы этой валюты
+        c_rate = response_json['rates'][to_valut]['rate']
+        # Результат (amount * c_rate)
+        result = round(float(response_json['rates'][to_valut]['rate_for_amount']), 2)
+
+        return render_template('converter/converter_page.html', title='Конвертер валюты', result=result, amount=c_amount, from_c_code=from_c_code, from_c_name=from_c_name, to_c_code=to_valut, to_c_name=to_c_name, time=update_date)
+
     return render_template('converter/converter_page.html', title='Конвертер валюты')
+
