69ab11712bfba693f11e490e4e15f1787b0b4138
DarkPythons <kuznec112233@mail.ru>
2024-08-24 22:33:53 +0500

    Add nice name in corrency handler

--- app/converter/router_converter.py
+++ app/converter/router_converter.py
@@ -13,30 +13,24 @@ def converter():
     if request.method == 'POST':
         flash('Конвертация прошла успешно', category='success')
         api_key = setting_currency.KEY_CURRENCY_API
-        from_valute = request.form['from_c']
-        to_valut = request.form['to_c']
-        amount = request.form['amount']
-        url = f'https://api.getgeoapi.com/v2/currency/convert?api_key={api_key}&from={from_valute}&to={to_valut}&amount={amount}&format=json'
+        form_from_currency = request.form['from_currency']
+        form_to_currency = request.form['to_c']
+        form_amount = request.form['amount']
+        url = f'https://api.getgeoapi.com/v2/currency/convert?api_key={api_key}&from={form_from_currency}&to={form_to_currency}&amount={form_amount}&format=json'
         response = requests.get(url, headers=setting_currency.HEADERS_BY_REQ)
         response_json = response.json()
-        # Код введенной валюты от которой должна быть конвертация
-        from_c_code = response_json['base_currency_code']
-        # Имя введеной валюты
-        from_c_name = response_json['base_currency_name']
-        # Введенное количество валюты
-        c_amount = response_json['amount']
-        c_amount = round(float(c_amount), 2)
-        # Последнее обновление данных
-        update_date = response_json['updated_date']
+        response_from_currency_code = response_json['base_currency_code']
+        response_from_currency_name = response_json['base_currency_name']
+        response_amount = response_json['amount']
+        response_amount_round = round(float(response_amount), 2)
+        last_update_date = response_json['updated_date']
 
-        # Имя валюты, в которую нужно было конвертировать
-        to_c_name = response_json['rates'][to_valut]['currency_name']
-        # Стоимость одной единицы этой валюты
-        c_rate = response_json['rates'][to_valut]['rate']
-        # Результат (amount * c_rate)
-        result = round(float(response_json['rates'][to_valut]['rate_for_amount']), 2)
+        response_to_currency_name = response_json['rates'][form_to_currency]['currency_name']
+        response_result_convert = response_json['rates'][form_to_currency]['rate_for_amount']
+        response_result_convert_round = round(float(response_result_convert), 2)
 
-        return render_template('converter/converter_page.html', title='Конвертер валюты', result=result, amount=c_amount, from_c_code=from_c_code, from_c_name=from_c_name, to_c_code=to_valut, to_c_name=to_c_name, time=update_date)
+
+        return render_template('converter/converter_page.html', title='Конвертер валюты', result=response_result_convert_round, amount=response_amount_round, from_c_code=response_from_currency_code, from_c_name=response_from_currency_name, to_c_code=form_to_currency, to_c_name=response_to_currency_name, time=last_update_date)
 
     return render_template('converter/converter_page.html', title='Конвертер валюты')
 
