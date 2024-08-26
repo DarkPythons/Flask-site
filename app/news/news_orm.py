d99026d1bce8f32770cab3ac2f14a1aa52697817
DarkPythons <kuznec112233@mail.ru>
2024-08-27 01:30:52 +0500

    Add news view pages

--- app/news/news_orm.py
+++ app/news/news_orm.py
@@ -5,9 +5,14 @@ class NewsOrm:
         from database import News, db
         self.db = db
         self.session = db.session
-        self.News = News
+        self.News: News = News
 
-    def get_ten_first_new(self):
-        query = select(self.News.id, self.News.title, self.News.text, self.News.views, self.News.photo, self.News.author_id).limit(10)
+    def get_first_news_orm(self):
+        query = select(self.News.id, self.News.title, self.News.text, self.News.views, self.News.photo, self.News.author_id, self.News.anons).limit(10)
         result = self.session.execute(query)
         return result.mappings().all()
+
+    def add_news_orm(self, *, anons, title, text, img, author_id):
+        news = self.News(anons=anons, title=title, text=text, photo=img, author_id=author_id)
+        self.session.add(news)
+        self.session.commit()
--- app/news/router_news.py
+++ app/news/router_news.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, request
-from flask_login import login_required
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_required, current_user
 
 from .news_orm import NewsOrm
 
@@ -13,13 +13,29 @@ news_router = Blueprint('news_router', __name__,
 @news_router.route('/')
 def news_page():
     new_orm = NewsOrm()
-    ten_news = new_orm.get_ten_first_new()
-    return render_template('news_page.html', title='Новостная страница')
+    news_list: list[dict, dict] = new_orm.get_first_news_orm()
+    return render_template('news_page.html', title='Новостная страница', news_data_list=news_list)
 
 @news_router.route('/add_news', methods=['POST', 'GET'])
 @login_required
 def add_news_page():
     if request.method == 'POST':
-        pass
+        new_orm = NewsOrm()
+        anons = request.form['anons']
+        title = request.form['title']
+        text = request.form['text']
+        photo = request.files.get('photo')
+        user_id = current_user.get_id()
+        img = None
+        if photo:
+            img = photo.read()
+
+        new_orm.add_news_orm(anons=anons, title=title, text=text, author_id=user_id, img=img)
+        return redirect(url_for('.news_page'))
 
     return render_template('add_news_page.html', title='Добавление статьи')
+
+@news_router.route('/view_news/<int:number_page>')
+@login_required
+def view_news_page(number_page):
+    return f"Страница номер {number_page}"
